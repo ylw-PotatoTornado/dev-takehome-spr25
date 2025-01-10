9a0b380059b15a9c79fd91c416f9da9ed447d2e9
ylw-PotatoTornado <yvwu510@gmail.com>
2025-01-09 23:33:32 -0500

    [NEW] Config DB Connection & DB Model

--- src/lib/types/request.ts
+++ src/lib/types/request.ts
@@ -4,3 +4,27 @@ export enum RequestStatus {
   COMPLETED = "completed",
   REJECTED = "rejected",
 }
+
+// Declare major request type involved in CRUD operation
+export interface ItemRequest {
+  id: number;
+  requestorName: string;
+  itemRequested: string;
+  requestCreatedDate: Date;
+  lastEditedDate: Date | null;
+  status: RequestStatus;
+}
+
+export interface CreateItemRequest {
+  requestorName: string;
+  itemRequested: string;
+}
+
+export interface EditStatusRequest {
+  id: number;
+  status: RequestStatus;
+}
+
+export interface DeleteRequests {
+  ids: number[];
+}
---
+++ src/server/db/config.ts
@@ -0,0 +1,44 @@
+import mongoose from  "mongoose";
+
+if (!process.env.MONGO_URI) {
+    throw new Error("Please define MongoDB connection string (MONGO_URI) in .env file.");
+}
+
+// Declare global with extra property to avoid multiple connections
+declare global {
+    var mongoose: {
+        conn: mongoose.Mongoose | null;
+        promise: Promise<mongoose.Mongoose> | null;
+    }
+}
+
+global.mongoose = global.mongoose || {conn: null, promise: null};
+
+
+export const connectDB = async (): Promise<mongoose.Mongoose> => {
+
+        if (global.mongoose.conn) {
+            console.log(`MongoDB connected: ${global.mongoose.conn.connection.host}`);
+            return global.mongoose.conn;
+        }
+
+        /** Estalibsh new connection when no prev connection exist */
+        if (!global.mongoose.promise) {
+            console.log("Creating new MongoDB connection.");
+
+            try {
+                global.mongoose.promise = mongoose.connect(process.env.MONGO_URI!, {
+                    bufferCommands: false,
+                });
+                
+                global.mongoose.conn = await global.mongoose.promise;
+                console.log(`MongoDB connected: ${global.mongoose.conn.connection.host}`);
+            } catch (error) {
+                console.error("Error connecting MongoDB", error);
+                throw new Error("Failed to connect to MongoDB");
+            }
+        }
+
+        return global.mongoose.conn;
+
+}
