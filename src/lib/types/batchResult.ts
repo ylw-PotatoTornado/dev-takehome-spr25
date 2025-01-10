e76391e61fa539930a896d595d53aed2151aecf4
ylw-PotatoTornado <yvwu510@gmail.com>
2025-01-09 23:39:41 -0500

    [NEW] Declare New Type batchResult

---
+++ src/lib/types/batchResult.ts
@@ -0,0 +1,38 @@
+
+import { EditStatusRequest } from "./request"
+
+/** Declare new type BatchResult to allow partial success in batch edit/deletes operations
+ * 
+ * Provide content for each success/fail status for better debugging
+*/
+
+
+
+export enum BatchStatus {
+    SUCCEED = "SUCCEED",
+    FAILED  = "FAILED",
+}
+
+export enum BatchError {
+    UNKNOWN_ERROR = "UNKNOWN_ERROR",
+    INVALID_INPUT = "INVALID_INPUT",
+}
+
+export type EditedContent = {
+    status:   BatchStatus,
+    body:  EditStatusRequest[],
+    error?:   BatchError,
+  }
+  
+export type DeletedContent = {
+    status:   BatchStatus,
+    body:  number[],            // array of ids
+    error?:   BatchError,
+  }
+  
+export type BatchResult = {
+    totalCount:     number
+    failedCount:    number,
+    succeedCount:   number,
+    content:        EditedContent[] | DeletedContent[],
+  }
