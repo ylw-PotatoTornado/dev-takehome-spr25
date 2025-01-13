daebc6313499de50c064f8f09a8b5d92443ebfde
ylw-PotatoTornado <yvwu510@gmail.com>
2025-01-13 00:39:50 -0500

    [UPDATE] Admin Page - Optimize table to be centered and responsive to screen.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -102,92 +102,92 @@ export default function ItemRequestsPage() {
 
 
   return (
+  <div className="flex justify-center items-center h-screen overflow-x-auto">
+      <div className="w-full max-w-[1205px] rounded-[8px] border-[1px] border-[#EAECF0]">
 
-    <div className="overflow-x-auto w-full max-w-[1205px] absolute top-[10%] left-[10%] rounded-[8px] border-[1px] border-[#EAECF0] ">
-      <TableHeader 
-        text= {tableHeader} 
-      />
+          <TableHeader 
+            text= {tableHeader} 
+          />
 
-      <TableTab
-        options={tabOptions}
-        selected={selectedTabStatus}
-        onChange={(newSelectedTabStatus) => 
-          setSelectedTabStatus(newSelectedTabStatus)
-        }
-      />
+          <TableTab
+            options={tabOptions}
+            selected={selectedTabStatus}
+            onChange={(newSelectedTabStatus) => 
+              setSelectedTabStatus(newSelectedTabStatus)
+            }
+          />
 
-      <table className="w-full max-w-[1205px] border-collapse">
-        
-        <thead>
-          <TableRowHeader headers={rowHeaders} />
-        </thead>
-
-
-        <tbody>
-          {paginatedRequests.map((request, index) => (
-           
-              <tr
-                className="h-[20px] border-b border-[#EAECF0] font-normal font-Inter text-sm text-[#667085] leading-[20px]"
-                key={request.id}
-              >
-                <td className="w-auto  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
-                  {request.requestorName}
-                </td>
-
-                <td className="w-[353px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
-                  {request.itemRequested}
-                </td>
-
-                <td className="w-[200px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
-                  {request.requestCreatedDate.toLocaleDateString(
-                    "en-US",
-                    dateFormatter,
-                  ) || ""}
-                </td>
-
-                <td className="w-[200px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
-                  {request.lastEditedDate?.toLocaleDateString(
-                    "en-US",
-                    dateFormatter,
-                  ) || ""}
-                </td>
-
-                <td className="w-[190px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px] relative">
-                  <Dropdown
-                    options={dropdownOptions}
-                    selected={rowStatus[index].status}
-                    onChange={(newStatus) =>
-                      handleDropdownChange(request.id, newStatus)
-                    }
-                  ></Dropdown>
-                </td>
-
-              </tr>
-            ))}
-        </tbody>
+          <table className="w-full max-w-[1205px] border-collapse">
+            
+            <thead>
+              <TableRowHeader headers={rowHeaders} />
+            </thead>
 
 
-        <tfoot>
-            <tr className="w-full">
-                <td className="w-full" colSpan={100}>
-                    <div className="flex items-center justify-between p-4">
-                        <div className="rectangle"></div>
-                        <div className="pagination">
-                            <Pagination
-                                pageNumber={pageNumber}
-                                pageSize={PAGINATION_PAGE_SIZE}
-                                totalRecords={totalRecords}
-                                onPageChange={(newPage) => setPageNumber(newPage)}
-                            />
+            <tbody>
+              {paginatedRequests.map((request, index) => (
+              
+                  <tr
+                    className="h-[20px] border-b border-[#EAECF0] font-normal font-Inter text-sm text-[#667085] leading-[20px]"
+                    key={request.id}
+                  >
+                    <td className="w-auto  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
+                      {request.requestorName}
+                    </td>
+
+                    <td className="w-[353px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
+                      {request.itemRequested}
+                    </td>
+
+                    <td className="w-[200px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
+                      {request.requestCreatedDate.toLocaleDateString(
+                        "en-US",
+                        dateFormatter,
+                      ) || ""}
+                    </td>
+
+                    <td className="w-[200px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px]">
+                      {request.lastEditedDate?.toLocaleDateString(
+                        "en-US",
+                        dateFormatter,
+                      ) || ""}
+                    </td>
+
+                    <td className="w-[190px]  pt-[12px] pb-[12px] pl-[24px] pr-[24px] gap-[12px] relative">
+                      <Dropdown
+                        options={dropdownOptions}
+                        selected={rowStatus[index].status}
+                        onChange={(newStatus) =>
+                          handleDropdownChange(request.id, newStatus)
+                        }
+                      ></Dropdown>
+                    </td>
+
+                  </tr>
+                ))}
+            </tbody>
+
+
+            <tfoot>
+                <tr className="w-full">
+                    <td className="w-full" colSpan={100}>
+                        <div className="flex items-center justify-between p-4">
+                            <div className="rectangle"></div>
+                            <div className="pagination">
+                                <Pagination
+                                    pageNumber={pageNumber}
+                                    pageSize={PAGINATION_PAGE_SIZE}
+                                    totalRecords={totalRecords}
+                                    onPageChange={(newPage) => setPageNumber(newPage)}
+                                />
+                            </div>
                         </div>
-                    </div>
-                </td>
-            </tr>
-        </tfoot>
+                    </td>
+                </tr>
+            </tfoot>
 
-
-      </table>
-
+          </table>
+      </div>
     </div>
 
   );
